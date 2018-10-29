28e4e97041632c09076fa370a5d2bdc693f7a086
daviskoh <hello@daviskoh.com>
2018-10-29 05:07:12 -0400

    recursive karatsuba

--- courses/01/assignments/karatsuba/multiply/karatsuba.go
+++ courses/01/assignments/karatsuba/multiply/karatsuba.go
@@ -1,31 +1,81 @@
 package multiply
 
 import (
+	"fmt"
 	"math/big"
+	"strconv"
 )
 
-// assume len(x) == len(y)
+func Pad(n string, by int, isLeft bool) string {
+	nCopy := n
 
-// (10*n)*ac + (10^(n/2))*(ad + bc) + bd
+	for i := 0; i < by; i++ {
+		if isLeft {
+			nCopy = fmt.Sprintf("0%s", nCopy)
+		} else {
+			nCopy = fmt.Sprintf("%s0", nCopy)
+		}
 
-func Karatsuba(x, y *big.Int) *big.Int {
-	a, _ := new(big.Int).SetString(x.String()[0:2], 10)
-	b, _ := new(big.Int).SetString(x.String()[2:4], 10)
+	}
 
-	c, _ := new(big.Int).SetString(y.String()[0:2], 10)
-	d, _ := new(big.Int).SetString(y.String()[2:4], 10)
+	return nCopy
+}
 
-	step1 := new(big.Int).Mul(a, c)
-	step2 := new(big.Int).Mul(b, d)
-	step3 := new(big.Int).Mul(a.Add(a, b), c.Add(c, d))
+func Karatsuba(x, y string) string {
+	xCopy := x
+	yCopy := y
 
-	step3copy := step3
-	step4sub1 := step3copy.Sub(step3copy, step2)
-	step4 := step4sub1.Sub(step4sub1, step1)
+	xLen := len(x)
+	yLen := len(y)
 
-	left, _ := new(big.Int).SetString(step1.String()+"0000", 10)
-	middle, _ := new(big.Int).SetString(step2.String(), 10)
-	right, _ := new(big.Int).SetString(step4.String()+"00", 10)
+	if xLen == 1 && yLen == 1 {
+		xInt, _ := strconv.Atoi(x)
+		yInt, _ := strconv.Atoi(y)
+		resp := strconv.Itoa(xInt * yInt)
+		return resp
+	}
 
-	return middle.Add(middle, left.Add(left, right))
+	if xLen < yLen {
+		xCopy = Pad(xCopy, yLen-xLen, true)
+	} else if yLen < xLen {
+		yCopy = Pad(yCopy, xLen-yLen, true)
+	}
+
+	digits := len(xCopy)
+	middle := digits / 2
+	if digits%2 != 0 {
+		middle++
+	}
+
+	a := xCopy[:middle]
+	b := xCopy[middle:]
+
+	c := yCopy[:middle]
+	d := yCopy[middle:]
+
+	ac, _ := new(big.Int).SetString(Karatsuba(a, c), 10)
+	bd, _ := new(big.Int).SetString(Karatsuba(b, d), 10)
+
+	aInt, _ := new(big.Int).SetString(a, 10)
+	bInt, _ := new(big.Int).SetString(b, 10)
+	cInt, _ := new(big.Int).SetString(c, 10)
+	dInt, _ := new(big.Int).SetString(d, 10)
+
+	aPlusB := new(big.Int).Add(aInt, bInt)
+	cPlusD := new(big.Int).Add(cInt, dInt)
+	abTimescd := new(big.Int)
+	abTimescd.SetString(Karatsuba(aPlusB.String(), cPlusD.String()), 10)
+
+	gaussSub := new(big.Int).Sub(abTimescd, ac)
+	gauss := new(big.Int).Sub(gaussSub, bd)
+
+	left, _ := new(big.Int).SetString(Pad(ac.String(), (digits-middle)*2, false), 10)
+	mid, _ := new(big.Int).SetString(Pad(gauss.String(), digits-middle, false), 10)
+
+	right := bd
+
+	respSub := new(big.Int).Add(left, mid)
+
+	resp := new(big.Int).Add(respSub, right).String()
+	return resp
 }
--- courses/01/assignments/karatsuba/multiply/karatsuba_test.go
+++ courses/01/assignments/karatsuba/multiply/karatsuba_test.go
@@ -4,25 +4,85 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"math/big"
-
 	. "github.com/daviskoh/algorithms_specialization/courses/01/assignments/karatsuba/multiply"
 )
 
 var _ = Describe("Karatsuba", func() {
-	It("handles base case", func() {
-		var x, y big.Int
+	var x, y string
 
-		x.SetString("5678", 10)
-		y.SetString("1234", 10)
-		Expect(Karatsuba(&x, &y).String()).To(Equal("7006652"))
+	// XContext("Pad()", func() {
+	// 	It("pads left", func() {
+	// 		Expect(Pad("1", 2, true)).To(Equal("001"))
+	// 		Expect(Pad("1", 10, true)).To(Equal("00000000001"))
+	// 	})
+	//
+	// 	It("pads right", func() {
+	// 		Expect(Pad("1", 2, false)).To(Equal("100"))
+	// 		Expect(Pad("1", 10, false)).To(Equal("10000000000"))
+	// 	})
+	// })
 
-		x.SetString("2233", 10)
-		y.SetString("4455", 10)
-		Expect(Karatsuba(&x, &y).String()).To(Equal("9948015"))
+	Context("Karatsuba()", func() {
+		It("handles simple cases", func() {
+			x = "12"
+			y = "1"
+			Expect(Karatsuba(x, y)).To(Equal("12"))
+		})
 
-		x.SetString("1423", 10)
-		y.SetString("5867", 10)
-		Expect(Karatsuba(&x, &y).String()).To(Equal("8348741"))
+		It("pads digits correctly", func() {
+			x = "123112"
+			y = "1231"
+			Expect(Karatsuba(x, y)).To(Equal("151550872"))
+
+			x = "12244482"
+			y = "1222"
+			Expect(Karatsuba(x, y)).To(Equal("14962757004"))
+
+			x = "7771829"
+			y = "77718291231"
+			Expect(Karatsuba(x, y)).To(Equal("604013269619531499"))
+
+			x = "314159265358979323846264338327"
+			y = "2718281828459045235360287471352662"
+			Expect(Karatsuba(x, y)).To(Equal("853973422267356706546355086952074141385326159404594475100076474"))
+		})
+
+		It("does thangs...", func() {
+			x = "2345"
+			y = "123"
+			Expect(Karatsuba(x, y)).To(Equal("288435"))
+
+			x = "1"
+			y = "123"
+			Expect(Karatsuba(x, y)).To(Equal("123"))
+
+			x = "12345"
+			y = "123"
+			Expect(Karatsuba(x, y)).To(Equal("1518435"))
+
+			x = "110"
+			y = "187"
+			Expect(Karatsuba(x, y)).To(Equal("20570"))
+
+			x = "4466"
+			y = "13354"
+			Expect(Karatsuba(x, y)).To(Equal("59638964"))
+
+			x = "11223344"
+			y = "55667788"
+			Expect(Karatsuba(x, y)).To(Equal("624778734443072"))
+
+			x = "12345678"
+			y = "12345678"
+			Expect(Karatsuba(x, y)).To(Equal("152415765279684"))
+
+			x = "99999999"
+			y = "99999999"
+			Expect(Karatsuba(x, y)).To(Equal("9999999800000001"))
+
+			x = "3141592653589793238462643383279502884197169399375105820974944592"
+			y = "2718281828459045235360287471352662497757247093699959574966967627"
+			Expect(Karatsuba(x, y)).To(Equal("8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184"))
+		})
 	})
 })
