d2d194dcd6fb421dc97dbe6a96db75eae1e05127
daviskoh <hello@daviskoh.com>
2018-11-13 00:44:59 -0500

    refactor to use indices instead of allocating left & right slices

--- courses/01/assignments/03_quicksort/sort/sort.go
+++ courses/01/assignments/03_quicksort/sort/sort.go
@@ -1,18 +1,17 @@
 package sort
 
-func ChoosePivotIndex(a []int) int {
-	length := len(a)
-
+func ChoosePivotIndex(a []int, l, r int) int {
 	first := 0
 
-	middle := length / 2
-	if length%2 == 0 {
+	middle := r / 2
+	if r%2 == 0 {
 		middle -= 1
 	}
 
-	last := length - 1
+	last := r - 1
 
-	sortedByValues, _ := Sort([]int{a[first], a[middle], a[last]}, func(a []int) int {
+	values := []int{a[first], a[middle], a[last]}
+	sortedByValues, _ := Sort(values, 0, 3, func(a []int, l, r int) int {
 		return 0
 	})
 
@@ -27,44 +26,47 @@ func ChoosePivotIndex(a []int) int {
 	return i
 }
 
-func Partition(a []int, pivotIndex int) int {
+func Partition(a []int, l, r int, pivotIndex int) int {
 	length := len(a)
 
 	if length == 1 {
 		return 0
 	}
 
-	a[pivotIndex], a[0] = a[0], a[pivotIndex]
+	if pivotIndex != 0 {
+		a[pivotIndex], a[0] = a[0], a[pivotIndex]
+	}
 
-	pivot := a[0]
-	i := 1
-	for j := i; j < length; j++ {
+	pivot := a[l]
+	i := l + 1
+	for j := l + 1; j < r; j++ {
 		if a[j] < pivot {
 			a[j], a[i] = a[i], a[j]
 			i++
 		}
 	}
 
-	a[0], a[i-1] = a[i-1], a[0]
+	a[l], a[i-1] = a[i-1], a[l]
 
 	return i - 1
 }
 
-func Sort(a []int, choosePivotIndex func(a []int) int) ([]int, int) {
-	if len(a) < 2 {
+var i = 0
+
+func Sort(a []int, l, r int, choosePivotIndex func(a []int, l, r int) int) ([]int, int) {
+	if r-l < 2 {
 		return a, 0
 	}
 
-	newPivotIndex := Partition(a, choosePivotIndex(a))
+	pivotIndex := choosePivotIndex(a, l, r)
+	currentPivotPos := Partition(a, l, r, pivotIndex)
 
 	comparisons := len(a) - 1
 
-	left := a[:newPivotIndex]
-	_, leftComparisons := Sort(left, choosePivotIndex)
+	_, leftComparisons := Sort(a, 0, currentPivotPos, choosePivotIndex)
 	comparisons += leftComparisons
 
-	right := a[newPivotIndex+1:]
-	_, rightComparisons := Sort(right, choosePivotIndex)
+	_, rightComparisons := Sort(a, currentPivotPos+1, r, choosePivotIndex)
 	comparisons += rightComparisons
 
 	return a, comparisons
--- courses/01/assignments/03_quicksort/sort/sort_test.go
+++ courses/01/assignments/03_quicksort/sort/sort_test.go
@@ -12,75 +12,101 @@ import (
 var _ = Describe("QuickSort", func() {
 	Context("ChoosePivotIndex()", func() {
 		var a []int
+		var l, r int
 
 		It("handles simple case odd length case", func() {
 			a = []int{1, 2, 3}
-			Expect(ChoosePivotIndex(a)).To(Equal(1))
+			l, r = 0, len(a)
+			Expect(ChoosePivotIndex(a, l, r)).To(Equal(1))
 
 			a = []int{1, 2, 3, 4, 6}
-			Expect(ChoosePivotIndex(a)).To(Equal(2))
+			l, r = 0, len(a)
+			Expect(ChoosePivotIndex(a, l, r)).To(Equal(2))
 
 			a = []int{6, 4, 7, 4, 2}
-			Expect(ChoosePivotIndex(a)).To(Equal(0))
+			l, r = 0, len(a)
+			Expect(ChoosePivotIndex(a, l, r)).To(Equal(0))
 		})
 
 		It("handles even lengths", func() {
 			a = []int{1, 2, 3, 4}
-			Expect(ChoosePivotIndex(a)).To(Equal(1))
+			l, r = 0, len(a)
+			Expect(ChoosePivotIndex(a, l, r)).To(Equal(1))
 
 			a = []int{1, 2, 3, 4, 6, 7}
-			Expect(ChoosePivotIndex(a)).To(Equal(2))
+			l, r = 0, len(a)
+			Expect(ChoosePivotIndex(a, l, r)).To(Equal(2))
 
 			a = []int{8, 2, 4, 5, 7, 1}
-			Expect(ChoosePivotIndex(a)).To(Equal(2))
+			l, r = 0, len(a)
+			Expect(ChoosePivotIndex(a, l, r)).To(Equal(2))
 		})
 	})
 
 	Context("Partition()", func() {
 		var a []int
+		var l, r int
 
 		It("handles simple case", func() {
+			a = []int{1}
+			l, r = 0, len(a)
+			Partition(a, l, r, 0)
+			Expect(a).To(Equal([]int{1}))
+
 			a = []int{1, 2, 3}
-			Partition(a, 0)
+			l, r = 0, len(a)
+			Partition(a, l, r, 0)
 			Expect(a).To(Equal([]int{1, 2, 3}))
 		})
 
 		It("partitions around a selected pivot index", func() {
 			a = []int{3, 1, 2}
-			Partition(a, 2)
+			l, r = 0, len(a)
+			Partition(a, l, r, 2)
 			Expect(a).To(Equal([]int{1, 2, 3}))
 		})
 
 		It("handles complex cases", func() {
 			a = []int{1, 8, 2, 5, 3, 4, 7, 6}
-			Partition(a, 4)
+			l, r = 0, len(a)
+			Partition(a, l, r, 4)
 			Expect(a).To(Equal([]int{1, 2, 3, 5, 8, 4, 7, 6}))
 		})
 	})
 
 	Context("Sort()", func() {
 		var a, resp, sorted []int
+		var l, r int
 		var comparisons int
 
 		It("handles base case", func() {
 			a = []int{1}
-			resp, _ = Sort(a, ChoosePivotIndex)
+			l, r = 0, len(a)
+			resp, _ = Sort(a, l, r, ChoosePivotIndex)
 			Expect(resp).To(Equal([]int{1}))
 		})
 
 		It("handles simple case", func() {
 			a = []int{2, 1}
-			resp, _ = Sort(a, ChoosePivotIndex)
+			l, r = 0, len(a)
+			resp, _ = Sort(a, l, r, ChoosePivotIndex)
 			Expect(resp).To(Equal([]int{1, 2}))
 		})
 
 		It("handles complex cases", func() {
 			a = []int{1, 3, 2}
-			resp, _ = Sort(a, ChoosePivotIndex)
+			l, r = 0, len(a)
+			resp, _ = Sort(a, l, r, ChoosePivotIndex)
 			Expect(resp).To(Equal([]int{1, 2, 3}))
 
+			a = []int{1, 3, 2, 5}
+			l, r = 0, len(a)
+			resp, _ = Sort(a, l, r, ChoosePivotIndex)
+			Expect(resp).To(Equal([]int{1, 2, 3, 5}))
+
 			a = []int{1, 7, 2, 5, 3, 4, 8, 6}
-			resp, _ = Sort(a, ChoosePivotIndex)
+			l, r = 0, len(a)
+			resp, _ = Sort(a, l, r, ChoosePivotIndex)
 			sorted = make([]int, len(a))
 			copy(sorted, a)
 			sort.Slice(sorted, func(i int, j int) bool {
@@ -89,7 +115,8 @@ var _ = Describe("QuickSort", func() {
 			Expect(resp).To(Equal(sorted))
 
 			a = []int{10, 5, 6, 4, 2, 7, 8, 9, 11, 14, 1}
-			resp, _ = Sort(a, ChoosePivotIndex)
+			l, r = 0, len(a)
+			resp, _ = Sort(a, l, r, ChoosePivotIndex)
 			sorted = make([]int, len(a))
 			copy(sorted, a)
 			sort.Slice(sorted, func(i int, j int) bool {
@@ -100,11 +127,13 @@ var _ = Describe("QuickSort", func() {
 
 		XIt("returns comparisons count", func() {
 			a = []int{1, 3, 2}
-			_, comparisons = Sort(a, ChoosePivotIndex)
+			l, r = 0, len(a)
+			_, comparisons = Sort(a, l, r, ChoosePivotIndex)
 			Expect(comparisons).To(Equal(3))
 
 			a = []int{4, 5, 6, 3}
-			_, comparisons = Sort(a, ChoosePivotIndex)
+			l, r = 0, len(a)
+			_, comparisons = Sort(a, l, r, ChoosePivotIndex)
 			Expect(comparisons).To(Equal(4))
 		})
 	})
